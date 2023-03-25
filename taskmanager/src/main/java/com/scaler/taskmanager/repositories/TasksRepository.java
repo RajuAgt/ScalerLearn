833a85e42ec6e97c50ce71d882d8a193072ece2d
RajuAgt <raju.agt501@gmail.com>
2023-03-25 12:56:24 +0530

    Repository Queries
    Signed-off-by: RajuAgt <raju.agt501@gmail.com>

--- taskmanager/src/main/java/com/scaler/taskmanager/repositories/TasksRepository.java
+++ taskmanager/src/main/java/com/scaler/taskmanager/repositories/TasksRepository.java
@@ -1,6 +1,10 @@
 package com.scaler.taskmanager.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.scaler.taskmanager.entities.TaskEntity;
@@ -8,4 +12,15 @@ import com.scaler.taskmanager.entities.TaskEntity;
 @Repository
 public interface TasksRepository extends JpaRepository<TaskEntity, Integer>{
 
+	List<TaskEntity> findAllByCompleted(boolean completed);
+	
+	@Query("SELECT t FROM tasks t WHERE t.completed = false AND t.dueDate < CURRENT_DATE")
+	List<TaskEntity> findAllOverdue();
+	
+	List<TaskEntity> findAllByCompletedAndDueDateBefore(boolean completed, Date dueDate);
+	
+	@Query("SELECT t FROM tasks t WHERE t.title %?1%")
+	List<TaskEntity> findAllByTitle();
+	
+	List<TaskEntity> findAllByTitleIsContainingIgnoreCase(String titleFragment);
 }
